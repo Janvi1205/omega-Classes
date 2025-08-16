30c968e5d540a1c1875c61b21da4438a8276d72a
Janvi1205 <janviyadav120505@gmail.com>
2025-08-16 16:22:09 +0530

    update

--- functions/config.js
+++ functions/config.js
@@ -3,7 +3,7 @@ module.exports = {
   // Email Configuration
   email: {
     // Brevo API Key (Sendinblue)
-    brevoApiKey: process.env.BREVO_API_KEY || "",
+    brevoApiKey: process.env.BREVO_API_KEY || "xsmtpsib-21182f1c2c60985a451d9d2a06f40effb920ce7d93541a7039cae9d1962e565b-DVkOz9sGbIfcx5KL",
     
     // Teacher's email address where registration notifications will be sent
     teacherEmail: process.env.TEACHER_EMAIL || "omegaproclasses@gmail.com",
